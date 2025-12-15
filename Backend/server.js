0b98ef45e8a5b757b2dd41b54d73d4599ed07928
Ayush Sharma <your-email@example.com>
2025-12-15 17:43:07 +0530

    ok ji

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Enable CORS - Allow all origins for development
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://gleeful-custard-ac881f.netlify.app",
   credentials: true
 }));
 
